1942cf44e7a0afebed40cb571b3d3c858c8a860a
Kai Yuan Neo <kai@rocketacademy.co>
2020-02-03 11:53:39 +0800

    Updates

---
+++ precache-manifest.5eed8c14aa94515961e7c6c84050e097.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c055c692fa39b84563ce1141bf5bf8",
+    "url": "/cra-deployment/index.html"
+  },
+  {
+    "revision": "b430959f440cb0969ef9",
+    "url": "/cra-deployment/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dc52d284cb218abd19c8",
+    "url": "/cra-deployment/static/js/2.bedd1424.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra-deployment/static/js/2.bedd1424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b430959f440cb0969ef9",
+    "url": "/cra-deployment/static/js/main.06227dff.chunk.js"
+  },
+  {
+    "revision": "ea5093d0cc9c728354b1",
+    "url": "/cra-deployment/static/js/runtime-main.358f0b1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra-deployment/static/media/logo.5d5d9eef.svg"
+  }
+]);
